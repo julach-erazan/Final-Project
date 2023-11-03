ea605a7ba68f5d02d9de570856088f67bc7c6fff
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2023-11-03 21:57:59 +0530

    update

--- frontEnd/reactjs/src/Pages/Home.js
+++ frontEnd/reactjs/src/Pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-full h-[100vh]'>
+    <div className='w-full h-[100vh] bg-black'>
       <h1>Hello</h1>
     </div>
   )
--- frontEnd/reactjs/src/component/Chatbot.js
+++ frontEnd/reactjs/src/component/Chatbot.js
@@ -6,61 +6,74 @@ const Chatbot = () => {
     setPopupWindow(!popupWindow)
   }
 
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-  const [editIndex, setEditIndex] = useState(-1);
+  const [imageInput, setImageInput] = useState(false);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const addTodo = () => {
-    if (inputValue.trim() !== '') {
-      if (editIndex === -1) {
-        setTodos([...todos, inputValue]);
-      } else {
-        const updatedTodos = [...todos];
-        updatedTodos[editIndex] = inputValue;
-        setTodos(updatedTodos);
-        setEditIndex(-1);
-      }
-      setInputValue('');
-    }
-  };
+  const cameraClick = () => {
+    setImageInput(!imageInput)
+  }
 
   return (
     <>
       <button onClick={handleClick}
-      className="w-[200px] h-[200px] fixed bottom-[30px] right-[30px] bg-cover
-      bg-[url('/src/component/chat-bot.gif')]
-      "></button>
+        className="w-[200px] h-[200px] fixed bottom-[30px] right-[30px] bg-cover
+        bg-[url('/src/component/chat-bot.gif')]
+        ">
+      </button>
 
       <div className={`
-      w-[100%] h-[100vh] fixed z-100 top-0 flex justify-center items-center bg-[#ffffffad]
-      ${(popupWindow === true) ? "visible" :"hidden"}
-      `}>
-        <div className='w-[300px] h-[600px] md:w-[400px] bg-[yellow]'>
-          <button onClick={handleClick} className="
-          w-[50px] h-[50px] relative top-[20px] right-[-230px] md:right-[-330px] 
-          bg-cover bg-[url('/src/component/close-button.png')]
-          "></button>
-    <div className="todo-input">
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button onClick={addTodo}>{editIndex === -1 ? 'Add' : 'Update'}</button>
-     </div>
-     <ul className="todo-list">
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {index === editIndex ? (
-              <input className="editInput" 
-              type="text" value={inputValue}
-              onChange={handleInputChange} />
-            ) : (
-              todo
-            )}
-          </li>
-        ))}
-     </ul>
+        w-[100%] h-[100vh] fixed z-100 top-0 flex justify-center items-center bg-[#ffffffad]
+        ${(popupWindow === true) ? "visible" :"hidden"}
+        `}>
+        <div className='
+          w-[300px] h-[600px] md:w-[400px] bg-[#e8e9eb] border-[10px] border-solid border-[#56cc36]'>
+          
+          <div className='w-[100%] h-[45px] bg-[#56cc36] flex items-center'>
+            <h3 className='text-[20px] font-black text-[#fff] w-[calc(100%-40px)] text-center'>ChatBot</h3>
+            <button onClick={handleClick} className="
+              w-[40px] h-[40px] 
+              bg-cover bg-[url('/src/component/back.png')]
+              ">
+            </button>
+          </div>
+          
+          <div className='w-[100%] h-[450px] bg-black mt-[20px] mb-[20px] pl-[10px] overflow-y-scroll'>
+              <div className='w-[300px] h-[70px] bg-[#fff] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[yellow] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[#fff] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[yellow] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[#fff] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[yellow] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[#fff] mt-[10px]'></div>
+              <div className='w-[300px] h-[70px] bg-[yellow] mt-[10px]'></div>
+          </div>
+          <div className={`
+          w-[380px] bg-[#bfc1c2] absolute z-200 bottom-[90px]
+          transition-all duration-500 ease-in
+          ${(imageInput === true) ? "h-[150px]" :"h-0"}
+          `}></div>
+          <div className='w-[100%] pl-[10px] pr-[10px]'>
+            <div className='w-[100%] h-[35px] rounded-l-[15px] rounded-r-[15px]
+              flex items-center
+            '>
+              <button onClick={cameraClick} className="
+                w-[15%] h-[40px] 
+                rounded-l-[15px] 
+                border-solid border-[#bfc1c2] border-l-[3px] border-t-[3px] border-b-[3px]
+                bg-cover bg-[url('/src/component/camera.png')]
+                "></button>
+              <input type={'text'} className='
+                pl-[15px]
+                focus:outline-none border-t-[3px] border-b-[3px] border-solid border-[#bfc1c2]
+                w-[70%]
+                h-[40px]
+              '></input>
+              <button className="
+                w-[15%] h-[40px] rounded-r-[15px] 
+                border-solid border-[#bfc1c2] border-r-[3px] border-t-[3px] border-b-[3px]
+                bg-cover bg-[url('/src/component/send.png')]
+                "></button>
+            </div>
+          </div>
         </div>
       </div>
     </>
